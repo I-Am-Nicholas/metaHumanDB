ea265a7e8782b7d0e4b9d1bcafa3d1a807f0851a
Nicholas Leacock <nicholas@swissmail.com>
2017-07-24 14:24:26 +0100

    Add new json-type module declaration to typings.d.ts Move meta-list to json file and format. Delete old meta-list file. Alter service and in-memory-data-sevice to take json object.

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,18 +1,16 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import metas from './metas.json'
+
 export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    let metalist = [
-      {id: 1, name: 'Iron Man', logo: 'assets/200s/faceplate.png', alias: 'Tony Stark' },
-      {id: 2, name: 'Captain America', logo: 'assets/200s/shield.png', alias: 'Steve Rogers' },
-      {id: 3, name: 'Hulk', logo: 'assets/200s/gamma.png', alias: 'Bruce Banner' },
-      {id: 4, name: 'Spider-Man', logo: 'assets/200s/spidey.png', alias: 'Peter Parker' },
-      {id: 5, name: 'Thor', logo: 'assets/200s/mjolnir.jpg', alias: 'God of Thunder' },
-      {id: 6, name: 'Black Panther', logo: 'assets/pantera.jpg', alias: "T'Challa, King of Wakanda" },
-      {id: 7, name: 'Scarlet Witch', logo: 'assets/scarlet.png', alias: 'Wanda Maximoff' },
-      {id: 8, name: 'Hawkeye', logo: 'assets/hawk.png', alias: 'Clint Barton' },
-      {id: 9, name: 'Black Widow', logo: 'assets/spider-bw.png', alias: 'Natasha Romanoff' },
-      {id: 10, name: 'Vision', logo: 'assets/gem.png', alias: 'Vision' }
-    ];
-    return {metalist};
+  createDb() {/*a method of InMemoryDataService that creates
+    a hash (collection names: collection objects)*/
+
+    let metalism = metas.metalist;/*the json file can be imported
+     using any name. If the data it contains is an array you need
+     no more code than: <let metalism = metas;>, but if the data is in object
+     form you must drill down to the object that directly references
+      the array: <metalism = metas.metalist;>*/
+
+    return {metalism};//used for the URL path.
   }
 }
--- src/app/meta-service.ts
+++ src/app/meta-service.ts
@@ -3,19 +3,22 @@ import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 import { Meta } from './meta';
-import { METAS } from './meta-list-local';
+
 
 @Injectable()
 export class MetaService {
 
-/*the metalistUrl variable is assigned the value that will be returned by the web url.*/
-  private metalistUrl = 'api/metalist';
+  private metalistUrl = 'api/metalism';
+
+/*the metalistUrl variable is assigned the value returned by the web url. At present,
+this particular value is being instructed to look in the InMemoryDataService class for a
+variable named 'metalism'*/
 
 /*injected Http client from an Angular library, giving access to http methods.*/
   constructor(private http: Http) { }
 
 
-/*This method expects an id as a parameter in the form of a number.
+/*The 'getMeta' method expects an id as a parameter in the form of a number.
 - It then sets up an instruction to search for that id within the
 array-like value of metalistUrl and to return the single value associated with
  that id, which is then assigned to a const url.
@@ -25,32 +28,27 @@ array-like value of metalistUrl and to return the single value associated with
 non-static asynchronous collection of flowing events not stored in memory.
 - This Observable is then turned into a Promise, the resolution of which is the
 conversion of the response into a json object of Meta type. */
+
   getMeta(id: number): Promise<Meta> {
-    const url = `${this.metalistUrl}/${id}`;
+    const url = '${this.metalistUrl}/${id}';
     return this.http.get(url)
       .toPromise()
       .then(response => response.json().data as Meta)
       .catch(this.handleError);
-    }
-      // getMeta(id: number): Promise<Meta> {
-      //   return this.getMetas()
-      //   .then(metas => metas.find(meta => meta.id === id));
-      // }
+  }
 
   /*Same as above except that this will return the entire list then resolve the
    promise by returning an array of Meta type. */
-    getMetas(): Promise<Meta[]> {
-      // return Promise.resolve(METAS);
-      return this.http.get(this.metalistUrl)
-                 .toPromise()
-                 .then(response => response.json().data as Meta[])
-                 .catch(this.handleError);
-    }
-
-    private handleError(error: any): Promise<any> {
-      console.error('An error occurred, Nicholas:', error);
-      return Promise.reject(error.message || error);
-    }
-
-
+  getMetas(): Promise<Meta[]> {
+    return this.http.get(this.metalistUrl)
+      .toPromise()
+      .then(response => response.json().data as Meta[])
+      .catch(this.handleError);
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred, Nicholas:', error);
+    return Promise.reject(error.message || error);
+  }
+
+}
