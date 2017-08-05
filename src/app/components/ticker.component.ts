0a297a09c837bed1cd2e250a9511c3c8a2c533ab
Nicholas Leacock <nicholas@swissmail.com>
2017-08-05 12:21:25 +0100

    Add Sass(scss) file to make ticker work. Add ticker content to template.

--- src/app/components/ticker.component.ts
+++ src/app/components/ticker.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ticker',
   templateUrl: '../templates/ticker.component.html',
-  styleUrls: ['../stylesheets/ticker.component.css']
+  styleUrls: ['../stylesheets/ticker.component.scss', '../sharedBG.css']
 })
 export class TickerComponent implements OnInit {
 
