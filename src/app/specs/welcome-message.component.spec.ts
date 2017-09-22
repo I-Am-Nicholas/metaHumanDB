35e208cf058f725b7195e9d3412ef56dc19855be
Nicholas Leacock <nicholas@swissmail.com>
2017-09-22 16:20:58 +0100

    Add unit test for presence of MiniIronMan div in DOM. Pass test by adding to DOM.

--- src/app/specs/welcome-message.component.spec.ts
+++ src/app/specs/welcome-message.component.spec.ts
@@ -1,9 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement} from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement} from "@angular/core";
 
-import { WelcomeMessageComponent } from '../components/welcome-message.component';
+import { WelcomeMessageComponent } from "../components/welcome-message.component";
 
-describe('WelcomeMessageComponent', () => {
+describe("WelcomeMessageComponent", () => {
   let component: WelcomeMessageComponent;
   let fixture: ComponentFixture<WelcomeMessageComponent>;
   let DOMElement:  DebugElement;
@@ -22,21 +22,28 @@ describe('WelcomeMessageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should be present in the DOM', () => {
-    expect(DOMElement[0].querySelectorAll('#welcome-message')).toBeTruthy();
+  it("should be present in the DOM", () => {
+    expect(DOMElement[0].querySelectorAll("#welcome-message")[0]).toBeTruthy();
+    // querySelectorAll returns a NodeList object, whether it locates the argument in the DOM
+    // or not, thereby always evaluating as truthy. For specificity we must request
+    // the first member of the object.
   });
 
-  it('should display text', () => {
-    let message = DOMElement[0].querySelectorAll('#welcome-message')
-    expect(message.textContent).not.toBe('');
+  it("should display text", () => {
+    let message = DOMElement[0].querySelectorAll("#welcome-message")[0]
+    expect(message.textContent).not.toBe("");
   });
 
-  fit("should initialize with a false value", () => {
+  it("should initialize with a false value", () => {
     expect(component.showAnimation).toBeFalsy();
+  });
+
+  it("should be present in DOM at initialization", ()=> {
+    expect(DOMElement[0].querySelectorAll("#mini-iron-man")[0]).toBeTruthy();
   });
 
 });
